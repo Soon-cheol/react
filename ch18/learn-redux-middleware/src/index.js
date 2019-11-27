a76149c0a675f2e0fd8b95571d0b879b98132581
Jiwon Park <jiwonpark@Jiwonui-MacBook-Pro.local>
2019-11-27 23:08:35 +0900

    ch18

--- ch18/learn-redux-middleware/src/index.js
+++ ch18/learn-redux-middleware/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import rootReducer from "./modules";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-const store = createStore(rootReducer, composeWithDevTools());
-
+import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 ReactDOM.render(
   <Provider store={store}>
     <App />
